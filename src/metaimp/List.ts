d6bf155484e3a426816f58087b0b5807eb57f2e8
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2020-05-16 12:08:36 +0200

    Document List

--- src/metaimp/List.ts
+++ src/metaimp/List.ts
@@ -71,11 +71,22 @@ export class List extends ListBase {
    * directly by other code, only when a property is evaluated.
    */
 
+  /**
+   * Appends the value to this list, returning the resulting list.
+   * @param data Value to append
+   * @returns New list
+   */
   private append(data: VmData): VmObject {
     let lst = new List([...this.value, data]);
     return new VmObject(lst);
   }
 
+  /**
+   * Appends the elements of the list to this list, returning a new list 
+   * consisting only of the unique elements of the combination.
+   * @param vmList List to append
+   * @returns New list
+   */
   private appendUnique(vmList: VmData): VmObject {
     let lst = vmList.unwrap();
     let values = (lst instanceof List) ? lst.getValue() : lst; 
@@ -101,16 +112,165 @@ export class List extends ListBase {
     return new VmObject(new List(this.value.slice(1)));
   }
 
-  private prepend(data: VmData): VmObject {
-    let lst = new List([data, ...this.value]);
-    return new VmObject(lst);
+  /**
+   * Creates a new list containing n elements by invoking the callback function once for 
+   * each element, and using the return value as the element value.
+   * @param vmFunc Callback function
+   * @param vmN Number of elements to generate
+   */
+  private generate(vmFunc: VmData, vmN: VmData) {
+    let n = vmN.unwrap();
+    if(n <= 0) n = 0;
+
+    let lst = [];
+    for(let i = 1; i <= n; i++) {
+      lst.push(vmFunc.invoke(new VmInt(i)).value);
+    }
+
+    return new VmObject(new List(lst));
   }
 
+  /**
+   * Returns a new list consisting of the unique elements of the original list. 
+   * @returns new List
+   */
   private getUnique(): VmObject {
     let value = this.makeUnique(this.value);
     return new VmObject(new List(value));    
   }
 
+  /**
+   * Returns a new list which results from inserting the given values (val1, val2, and so on) 
+   * into the existing list before the element at the position given by index.
+   * @param vmIndex Index to insert at
+   * @param args Values
+   */
+  private insertAt(vmIndex: VmInt, ...args: VmData[]) : VmObject {
+    let idx = this.unwrapIndex(vmIndex);
+    let newValue = this.value.slice(0);
+    newValue.splice(idx, 0, ...args);
+    return new VmObject(new List(newValue));
+  }
+
+  /**
+   * Returns a new list consisting of the intersection of this list and another list.
+   * @param vmList List to intersect with
+   * @returns new List
+   */
+  private intersect(vmLst: VmObject) {
+    // Convert argument to simple array:
+    let arg = vmLst.unwrap();
+    let lst2: VmData[] = ((arg instanceof List) ? arg.getValue() : arg); 
+
+    // Determine which is the shorter list:
+    let short = this.value;
+    let long = lst2;
+    if(this.value.length > lst2.length) {
+      long = this.value;
+      short = lst2;
+    }
+
+    let result: VmData[];
+    result = short.filter((x) => {
+      return long.find((y) => x.eq(y));
+    });
+
+    return new VmObject(new List(result));
+  }
+
+  /**
+   * For each element of the list, this method invokes the callback function, 
+   * passing the current element as the single argument, then adds the callback's 
+   * return value to a new list.
+   * @param vmFunc Callback function
+   * @returns new List
+   */
+  private mapAll(vmFunc: VmData): VmObject {
+    let lst = this.value.map((x) => vmFunc.invoke(x));
+    return new VmObject(new List(lst));
+  }
+
+  /**
+   * Returns a new list which results from inserting the value before 
+   * the first element of the existing list. 
+   * @param val Value to prepend
+   * @returns New List
+   */
+  private prepend(val: VmData): VmObject {
+    let lst = new List([val, ...this.value]);
+    return new VmObject(lst);
+  }
+
+  /**
+   * Returns a new list which results from deleting the element at the given index. 
+   * @param vmIdx Index of element to remove
+   * @returns New List
+   */
+  private removeElementAt(vmIdx: VmInt): VmObject {
+    let idx = this.unwrapIndex(vmIdx);
+    if(idx < 0 || idx > this.value.length-1) throw('index out of range');
+    let newValue = this.value.slice();
+    newValue.splice(idx, 1);
+    return new VmObject(new List(newValue));
+  }
+
+  /**
+   * Returns a new list which results from deleting a range of elements.
+   * @param vmStartIndex Start index
+   * @param vmEndIndex End index (inclusive)
+   * @returns New List
+   */
+  private removeRange(vmStartIndex: VmInt, vmEndIndex: VmInt): VmObject {
+    let startIdx = this.unwrapIndex(vmStartIndex);
+    let endIdx = this.unwrapIndex(vmEndIndex);
+    if(startIdx < 0 || startIdx > this.value.length-1) throw('index out of range');
+    if(endIdx < 0 || endIdx > this.value.length-1) throw('index out of range');
+    if(endIdx < startIdx) throw('End index must be greater than or equal to start index');
+    let count = endIdx - startIdx + 1;
+    let newValue = this.value.slice();
+    newValue.splice(startIdx, count);
+    return new VmObject(new List(newValue));
+  }
+
+  /**
+   * Returns a new list consisting of the elements of this list rearranged into a sorted order.
+   * @param vmDescending `true` if descending, `nil` if ascending
+   * @param vmFunc Optional comparison function
+   * @returns New List
+   */
+  private sort(vmDescending?: VmData, vmFunc?: VmData): VmObject {
+    // Descending is false if unspecified, or VmNil, or VmInt=0
+    let descending = (!vmDescending || vmDescending.isFalsy()) ? false : true;
+
+    // Make a copy, because sort happens in place.
+    let lst = this.value.slice();
+
+    // Sort in place, either by comparing elements or by calling 
+    // a callback with two arguments.
+    lst.sort((a: VmData, b: VmData) => {
+      if(vmFunc) {
+        return vmFunc.invoke(a,b).value;;
+      } else {
+        return a.lt(b) ? -1 : 1;
+      }
+    });
+
+    // If descending, reverse result in place.
+    if(descending) lst.reverse();
+
+    // Return result as new Metalist:
+    return new VmObject(new List(lst));
+  }  
+
+
+  /**
+   * Returns a new list which results from deleting elements starting at 
+   * startIndex, then inserting any additional argument values as new list elements in their place. 
+   * @param vmStartIndex Start index
+   * @param vmDeleteCount Number of elements to delete
+   * @param args Values to insert
+   * @returns New List
+   */
   private splice(vmStartIndex: VmInt, vmDeleteCount: VmInt, ...args: VmData[]): VmObject {
     let idx = this.unwrapIndex(vmStartIndex);
     let deleteCount = vmDeleteCount.unwrap();
@@ -119,6 +279,12 @@ export class List extends ListBase {
     return new VmObject(new List(values));
   }
 
+  /**
+   * Creates and returns a new list consisting of a sublist of this list.
+   * @param vmStartIndex Start index
+   * @param vmLength Length of sublist
+   * @returns New List
+   */
   private sublist(vmStartIndex: VmInt, vmLength?: VmInt) : VmObject {
     let start = vmStartIndex.unwrap() - 1;
     let length = vmLength ? vmLength.unwrap() : null;
@@ -147,33 +313,11 @@ export class List extends ListBase {
     return new VmObject(new List(out));
   }
 
-  private removeElementAt(vmIdx: VmInt): VmObject {
-    let idx = this.unwrapIndex(vmIdx);
-    if(idx < 0 || idx > this.value.length-1) throw('index out of range');
-    let newValue = this.value.slice();
-    newValue.splice(idx, 1);
-    return new VmObject(new List(newValue));
-  }
 
-  private removeRange(vmStartIndex: VmInt, vmEndIndex: VmInt): VmObject {
-    let startIdx = this.unwrapIndex(vmStartIndex);
-    let endIdx = this.unwrapIndex(vmEndIndex);
-    if(startIdx < 0 || startIdx > this.value.length-1) throw('index out of range');
-    if(endIdx < 0 || endIdx > this.value.length-1) throw('index out of range');
-    if(endIdx < startIdx) throw('End index must be greater than or equal to start index');
-    let count = endIdx - startIdx + 1;
-    let newValue = this.value.slice();
-    newValue.splice(startIdx, count);
-    return new VmObject(new List(newValue));
-  }
-
-  private insertAt(vmIndex: VmInt, ...args: VmData[]) : VmObject {
-    let idx = this.unwrapIndex(vmIndex);
-    let newValue = this.value.slice(0);
-    newValue.splice(idx, 0, ...args);
-    return new VmObject(new List(newValue));
-  }
-
+  /**
+   * 
+   * @param vmFunc 
+   */
   private subset(vmFunc: VmData): VmObject {
     let lst: VmData[] = [];
     lst = this.value.filter((x) => {
@@ -183,82 +327,10 @@ export class List extends ListBase {
     return new VmObject(new List(lst));
   }
 
-  private mapAll(vmFunc: VmData): VmObject {
-    let lst = this.value.map((x) => {
-      return vmFunc.invoke(x);
-    });
-    return new VmObject(new List(lst));
-  }
 
-  private sort(vmDescending?: VmData, vmFunc?: VmData): VmObject {
-    // Descending is false if unspecified, or VmNil, or VmInt=0
-    let descending = (!vmDescending || vmDescending.isFalsy()) ? false : true;
 
-    // Make a copy, because sort happens in place.
-    let lst = this.value.slice();
 
-    // Sort in place, either by comparing elements or by calling 
-    // a callback with two arguments.
-    lst.sort((a: VmData, b: VmData) => {
-      if(vmFunc) {
-        return vmFunc.invoke(a,b).value;;
-      } else {
-        return a.lt(b) ? -1 : 1;
-      }
-    });
 
-    // If descending, reverse result in place.
-    if(descending) lst.reverse();
-
-    // Return result as new Metalist:
-    return new VmObject(new List(lst));
-  }
-
-  private generate(vmFunc: VmData, vmN: VmData) {
-    let n = vmN.unwrap();
-    if(n <= 0) n = 0;
-
-    let lst = [];
-    for(let i = 1; i <= n; i++) {
-      lst.push(vmFunc.invoke(new VmInt(i)).value);
-    }
-
-    return new VmObject(new List(lst));
-  }
-
-  /**
-   * Returns a new list consisting of the intersection of this list and lst2; 
-   * that is, a list consisting of the elements common to both this list and lst2. 
-   *  
-   * If the two lists have no elements in common, the result is an empty list. 
-   * 
-   * If an element of the shorter list (or, if the lists are of equal length, this list)
-   * appears more than once in the shorter list, and that element value also appears in 
-   * the longer list, then the element will be in the result list the same number of times 
-   * that it is in the shorter list. 
-   *  
-   * An element repeated in the longer list will not be repeated in the result list.     
-   */
-  private intersect(vmLst: VmObject) {
-    // Convert argument to simple array:
-    let arg = vmLst.unwrap();
-    let lst2: VmData[] = ((arg instanceof List) ? arg.getValue() : arg); 
-
-    // Determine which is the shorter list:
-    let short = this.value;
-    let long = lst2;
-    if(this.value.length > lst2.length) {
-      long = this.value;
-      short = lst2;
-    }
-
-    let result: VmData[];
-    result = short.filter((x) => {
-      return long.find((y) => x.eq(y));
-    });
-
-    return new VmObject(new List(result));
-  }
 
   getValue() {
     return this.value;
