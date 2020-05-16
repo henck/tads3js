48b3994ee91ed415ce52944229ee96ca797290bc
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2020-05-16 15:04:49 +0200

    ByteArray constructor

--- src/metaimp/ByteArray.ts
+++ src/metaimp/ByteArray.ts
@@ -1,21 +1,50 @@
 import { Metaclass, TPropFunc } from '../metaclass/Metaclass';
 import { MetaclassRegistry } from '../metaclass/MetaclassRegistry'
 
-import { VmData, VmInt } from "../types";
+import { VmData, VmInt, VmObject } from "../types";
 import { SourceImage } from "../SourceImage";
 import { Pool } from "../Pool";
+import { MetaString } from './MetaString';
 
 
 
 export class ByteArray extends Metaclass {
   private value: number[];
 
-  constructor(str?: string) {
+  // new ByteArray(native str)
+  // new ByteArray(vmInt)
+  // new ByteArray(vmStr)
+  // new ByteArray(vmStr, vmStr)
+  // new ByteArray(ByteArray, vmInt, vmInt)
+  constructor(...args: any[]) {
     super();
     this.value = [];
-    if(str) {
-      for(let i = 0; i < str.length; i++) {
-        this.value.push(str.charCodeAt(i));
+
+    if(args.length == 0) throw('ByteArray: not enough arguments');
+
+    // If a native string, convert it to a MetaString.
+    if(typeof(args[0]) == 'string') {
+      args[0] = new VmObject(new MetaString(args[0]));
+    }
+
+    let arg0 = args[0].unpack();
+    // If a number, then create an empty ByteArray with _n_ 
+    // elements of value 0.
+    if(typeof(arg0) == 'number') {
+      this.value = this.value.fill(0, 0, arg0);
+    }
+    // If a string, create a ByteArray with Unicode chars 
+    // from string.
+    else if(typeof(arg0) == 'string') {
+      this.value = arg0.split('').map((c) => c.charCodeAt(0));
+    } 
+    // If an array (another ByteArray), then make a copy
+    // of a segment of it.
+    else if(Array.isArray(arg0)) {
+      let start = args[1].unpack();
+      let len = args[2].unpack();
+      for(let i = 0; i < len; i++) {
+        this.value.push(arg0[start+i]);
       }
     }
   }
@@ -23,6 +52,10 @@ export class ByteArray extends Metaclass {
   static loadFromImage(image: SourceImage, dataPool: Pool, offset: number) {
     throw('ByteArray: Cannot load from image');
   }  
+
+  unpack(): any {
+    return this.value;
+  }
 
   getMethodByIndex(idx: number): TPropFunc {
     switch(idx) {
