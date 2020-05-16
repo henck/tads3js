ce0eb53bf0389ba72604c9d285ddb24719578dd4
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2020-05-16 15:27:41 +0200

    Add ByteArray.copyFrom

--- src/metaimp/ByteArray.ts
+++ src/metaimp/ByteArray.ts
@@ -57,9 +57,14 @@ export class ByteArray extends Metaclass {
     return this.value;
   }
 
+  getValue(): any {
+    return this.value;
+  }
+
   getMethodByIndex(idx: number): TPropFunc {
     switch(idx) {
       case 0: return this.length;
+      case 2: return this.copyFrom;
     }
     return null;
   }  
@@ -74,7 +79,32 @@ export class ByteArray extends Metaclass {
    * Meta methods - all private as they should not be called
    * directly by other code, only when a property is evaluated.
    */
+
+  /**
+   * Copies bytes from sourceArray, which must be another ByteArray object.
+   * @param vmSourceArray Source ByteArray
+   * @param vmSourceIndex Source index
+   * @param vmDestIndex Destination index
+   * @param vmLen Number of bytes to copy.
+   */
+  private copyFrom(vmSourceArray: VmData, vmSourceIndex: VmInt, vmDestIndex: VmInt, vmLen: VmInt): VmData {
+    let sourceArray = vmSourceArray.unpack();
+    let sourceIndex = vmSourceIndex.unpack() - 1;
+    let destIndex = vmDestIndex.unpack() - 1;
+    let len = vmLen.unpack();
+
+    for(let i = 0; i < len; i++) {
+      this.value[i + destIndex] = sourceArray[i + sourceIndex];
+    }
+    
+    return null;
+  }
   
+  /**
+   * Returns the number of bytes in the ByteArray. This is the 
+   * same as the size specified when the object was created. 
+   * @returns Length of ByteArray
+   */
   private length(): VmData {
     return new VmInt(this.value.length);
   }
