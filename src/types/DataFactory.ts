cb62e7c4c53846d91f01539b525032928f724cb2
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2020-05-23 13:40:33 +0200

    Fix read obj from pool

--- src/types/DataFactory.ts
+++ src/types/DataFactory.ts
@@ -3,21 +3,21 @@ import { Pool } from '../Pool';
 import { VmData, VmNil, VmTrue, VmObject, VmInt, VmSstring, VmList, VmCodeOffset, VmDstring, VmProp } from './';
 
 export class DataFactory {
-  static load(num: VmType, dataPool: Pool, offset: number): VmData {
+  static load(num: VmType, dataPool: Pool, offsetOrValue: number): VmData {
     switch(num) {
       case VmType.NIL: return new VmNil();
       case VmType.TRUE: return new VmTrue();
-      case VmType.OBJ: return new VmObject(dataPool.getUint4(offset));
+      case VmType.OBJ: return new VmObject(offsetOrValue);
       case VmType.INT: 
         // Very weird INT encoding seems to use only 24 bits:
-        let val = offset;
+        let val = offsetOrValue;
         if((val >> 24) == 1) val = val - 16777471;
         return new VmInt(val);
-      case VmType.DSTRING: return new VmDstring(dataPool.getString(offset));
-      case VmType.SSTRING: return new VmSstring(dataPool.getString(offset));
-      case VmType.LIST: return new VmList(dataPool.getList(offset));
-      case VmType.CODEOFS: return new VmCodeOffset(offset);
-      case VmType.PROP: return new VmProp(dataPool.getUint2(offset));
+      case VmType.DSTRING: return new VmDstring(dataPool.getString(offsetOrValue));
+      case VmType.SSTRING: return new VmSstring(dataPool.getString(offsetOrValue));
+      case VmType.LIST: return new VmList(dataPool.getList(offsetOrValue));
+      case VmType.CODEOFS: return new VmCodeOffset(offsetOrValue);
+      case VmType.PROP: return new VmProp(dataPool.getUint2(offsetOrValue));
       default:
         throw('Cannot create type ' + num.toString());
     }    
