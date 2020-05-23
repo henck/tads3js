d93af9aa9bc4372d4916f68b06c911e5b89ef588
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2020-05-23 19:51:03 +0200

    Add VM_FUNCPTR loader

--- src/types/DataFactory.ts
+++ src/types/DataFactory.ts
@@ -1,19 +1,20 @@
 import { VmType } from './VmType'
 import { Pool } from '../Pool';
-import { VmData, VmNil, VmTrue, VmObject, VmInt, VmSstring, VmList, VmCodeOffset, VmDstring, VmProp } from './';
+import { VmData, VmNil, VmTrue, VmObject, VmInt, VmSstring, VmList, VmCodeOffset, VmDstring, VmProp, VmFuncPtr } from './';
 
 export class DataFactory {
   static load(num: VmType, dataPool: Pool, offsetOrValue: number): VmData {
     switch(num) {
-      case VmType.NIL: return new VmNil();
-      case VmType.TRUE: return new VmTrue();
-      case VmType.OBJ: return new VmObject(offsetOrValue);
-      case VmType.INT: return new VmInt(offsetOrValue); // Oddly, there is no need to turn this into a signed int
+      case VmType.NIL:     return new VmNil();
+      case VmType.TRUE:    return new VmTrue();
+      case VmType.OBJ:     return new VmObject(offsetOrValue);
+      case VmType.INT:     return new VmInt(offsetOrValue); // Oddly, there is no need to turn this into a signed int
       case VmType.DSTRING: return new VmDstring(dataPool.getString(offsetOrValue));
       case VmType.SSTRING: return new VmSstring(dataPool.getString(offsetOrValue));
-      case VmType.LIST: return new VmList(dataPool.getList(offsetOrValue));
+      case VmType.LIST:    return new VmList(dataPool.getList(offsetOrValue));
       case VmType.CODEOFS: return new VmCodeOffset(offsetOrValue);
-      case VmType.PROP: return new VmProp(offsetOrValue & 0xffff); // only first two bytes are used, last two are arbitrary
+      case VmType.PROP:    return new VmProp(offsetOrValue & 0xffff); // only first two bytes are used, last two are arbitrary
+      case VmType.FUNCPTR: return new VmFuncPtr(offsetOrValue);
       default:
         throw('Cannot create type ' + num.toString());
     }    
