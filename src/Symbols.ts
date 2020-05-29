f1bf7aa278902cec098e7137f7ec1ec0dbeee23c
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2020-05-29 13:16:13 +0200

    Add symbols found in adv3 game

--- src/Symbols.ts
+++ src/Symbols.ts
@@ -13,6 +13,13 @@ export type TSymbol =
   | 'GrammarProd.grammarInfo'
   | 'GrammarProd.grammarTag'
   | 'GrammarProd.miscVocab'
+  | 'GrammarProd.firstTokenIndex'
+  | 'GrammarProd.lastTokenIndex'
+  | 'GrammarProd.tokenList'
+  | 'GrammarProd.tokenMatchList'
+  | 'GrammarProd.altProps'
+  | 'GrammarProd.GrammarAltInfo'
+  | 'GrammarProd.GrammarAltTokInfo'
   | 'operator +'
   | 'operator -'
   | 'operator *'
@@ -27,7 +34,25 @@ export type TSymbol =
   | 'operator &'
   | 'operator negate'
   | 'operator []'
-  | 'operator []=';
+  | 'operator []='
+  | 'mainRestore'
+  | 'RuntimeError'
+  | 'exceptionMessage'
+  | 'CharacterSet.UnknownCharSetException'
+  | 'StorageServerError'
+  | 'T3StackInfo'
+  | 'FileSpec.getFilename'
+  | 'FileSpec.closeFile'
+  | 'File.FileInfo'
+  | 'File.FileNotFoundException'
+  | 'File.FileCreationException'
+  | 'File.FileOpenException'
+  | 'File.FileIOException'
+  | 'File.FileSyncException'
+  | 'File.FileClosedException'
+  | 'File.FileModeException'
+  | 'File.FileSafetyException'
+  | 'propNotDefined';
 
 export class Symbols {
   private static sym: Map<TSymbol, VmData> = new Map<TSymbol, VmData>();
@@ -46,6 +71,13 @@ export class Symbols {
       case 'GrammarProd.grammarInfo': return 'GrammarProd.grammarInfo';
       case 'GrammarProd.grammarTag': return 'GrammarProd.grammarTag';
       case 'GrammarProd.miscVocab': return 'GrammarProd.miscVocab';
+      case 'GrammarProd.firstTokenIndex': return 'GrammarProd.firstTokenIndex';
+      case 'GrammarProd.lastTokenIndex': return 'GrammarProd.lastTokenIndex';      
+      case 'GrammarProd.tokenList': return 'GrammarProd.tokenList';
+      case 'GrammarProd.tokenMatchList': return 'GrammarProd.tokenMatchList';
+      case 'GrammarProd.altProps': return 'GrammarProd.altProps';
+      case 'GrammarProd.GrammarAltInfo': return 'GrammarProd.GrammarAltInfo';
+      case 'GrammarProd.GrammarAltTokInfo': return 'GrammarProd.GrammarAltTokInfo';
       case 'operator +': return 'operator +';
       case 'operator -': return 'operator -';
       case 'operator *': return 'operator *';
@@ -61,6 +93,24 @@ export class Symbols {
       case 'operator negate': return 'operator negate';
       case 'operator []': return 'operator []';
       case 'operator []=': return 'operator []=';
+      case 'mainRestore': return 'mainRestore';
+      case 'RuntimeError': return 'RuntimeError';
+      case 'exceptionMessage': return 'exceptionMessage';
+      case 'CharacterSet.UnknownCharSetException': return 'CharacterSet.UnknownCharSetException';
+      case 'StorageServerError': return 'StorageServerError';
+      case 'T3StackInfo': return 'T3StackInfo';
+      case 'FileSpec.getFilename': return 'FileSpec.getFilename';
+      case 'FileSpec.closeFile': return 'FileSpec.closeFile';
+      case 'File.FileInfo': return 'File.FileInfo';
+      case 'File.FileNotFoundException': return 'File.FileNotFoundException';
+      case 'File.FileCreationException': return 'File.FileCreationException';
+      case 'File.FileOpenException': return 'File.FileOpenException';
+      case 'File.FileIOException': return 'File.FileIOException';
+      case 'File.FileSyncException': return 'File.FileSyncException';
+      case 'File.FileClosedException': return 'File.FileClosedException';
+      case 'File.FileModeException': return 'File.FileModeException';
+      case 'File.FileSafetyException': return 'File.FileSafetyException';
+      case 'propNotDefined': return 'propNotDefined';
       default: 
         throw(`Unknown symbol: ${name}`);
     }
