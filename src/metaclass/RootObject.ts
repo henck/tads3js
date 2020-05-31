5d320080a214275e7825a27c040dc0b03a9caf73
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2020-05-31 18:32:10 +0200

    Add metafunctions names to metafunctions

--- src/metaclass/RootObject.ts
+++ src/metaclass/RootObject.ts
@@ -60,15 +60,15 @@ class RootObject {
 
   getMethodByIndex(idx: number): VmNativeCode {
     switch(idx) {
-      case 0: return new VmNativeCode(this.ofKind, 1);
-      case 1: return new VmNativeCode(this.metaGetSuperclassList, 0);
-      case 2: return new VmNativeCode(this.propDefined, 1, 1);
-      case 3: return new VmNativeCode(this.propType, 1);
-      case 4: return new VmNativeCode(this.getPropList, 0);
-      case 5: return new VmNativeCode(this.getPropParams, 1);
-      case 6: return new VmNativeCode(this.metaIsClass, 0);
+      case 0: return new VmNativeCode("RootObject.ofKind", this.ofKind, 1);
+      case 1: return new VmNativeCode("RootObject.getSuperclassList", this.metaGetSuperclassList, 0);
+      case 2: return new VmNativeCode("RootObject.propDefined", this.propDefined, 1, 1);
+      case 3: return new VmNativeCode("RootObject.propType", this.propType, 1);
+      case 4: return new VmNativeCode("RootObject.getPropList", this.getPropList, 0);
+      case 5: return new VmNativeCode("RootObject.getPropParams", this.getPropParams, 1);
+      case 6: return new VmNativeCode("RootObject.isClass", this.metaIsClass, 0);
       // propInherited (multimethod only)
-      case 8: return new VmNativeCode(this.isTransient, 0);
+      case 8: return new VmNativeCode("RootObject.isTransient", this.isTransient, 0);
     }
     return null;
   }  
