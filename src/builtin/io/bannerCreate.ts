91818d82a12301f451852ad5159dda9ca4f385e3
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2020-06-01 17:59:58 +0200

    Remove test msg

--- src/builtin/io/bannerCreate.ts
+++ src/builtin/io/bannerCreate.ts
@@ -4,7 +4,6 @@ export function builtin_bannerCreate(
     vmParent: VmInt, vmWhere: VmInt, vmOther: VmInt, 
     vmWindowType: VmInt, vmAlign: VmInt, vmSize: VmInt, vmSizeUnits: VmInt, vmStyle: VmInt): VmData 
 {
-  console.log("bannerCreate");
   return new VmInt(0);
 }
 
