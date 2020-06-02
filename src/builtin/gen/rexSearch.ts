5017ab8988752136781e8ce42ae0a45a5e1e6767
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2020-06-02 16:40:21 +0200

    Documentation

--- src/builtin/gen/rexSearch.ts
+++ src/builtin/gen/rexSearch.ts
@@ -1,6 +1,13 @@
 import { VmData, VmInt, VmNil, VmSstring, VmList } from "../../types";
 import { RexPattern } from "../../metaimp";
 
+/**
+ * Searches for the regular expression pat in the search string str, starting at the character position index.
+  * @param vmPat Pattern to search for (string or RexPattern)
+ * @param vmStr String to search in
+ * @param vmIndex Index to start at
+ * @returns list with match info, or nil for no match
+ */
 export function builtin_rexSearch(vmPat: VmData, vmStr: VmData, vmIndex?: VmInt): VmData {
   let pat = vmPat.unpack(); // rx is a string or a RexPattern instance
   // If a string was given, create a RexPattern.
